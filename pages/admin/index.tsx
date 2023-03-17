2b8046553704e901094590f3b47643c9b031604b
Vinicius <viniciuspsilvas@gmail.com>
2023-03-17 16:35:26 +1000

    Setup Admin index page

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,15 +1,32 @@
+import { Typography } from '@mui/material'
 import { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
 import { AdminLayout } from './AdminLayout'
 
 const AdminPage: NextPage = () => {
   const { data: session } = useSession()
+  const user = session?.user
+
+  if (!user) return <>You are not logged in.</>
 
   return (
     <AdminLayout>
-      <h2>Super-secret admin stuff for Super Users.</h2>
-
-      <> {`I am the user ${session?.user.firstname}`}</>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        Admin page
+      </Typography>
+      <br />
+      <div className="flex space-x-2 ">
+        <div>Firstname:</div>
+        <div className="font-semibold">{user.firstname}</div>
+      </div>
+      <div className="flex space-x-2 ">
+        <div>Lastname:</div>
+        <div className="font-semibold">{user.lastname}</div>
+      </div>
+      <div className="flex space-x-2 ">
+        <div>Email:</div>
+        <div className="font-semibold">{user.email}</div>
+      </div>
     </AdminLayout>
   )
 }
