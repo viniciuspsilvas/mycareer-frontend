0ed8b4314925bf934fa198af882fbc74ed107bd0
Vinicius <viniciuspsilvas@gmail.com>
2023-03-18 09:52:57 +1000

    Setup AdminLayout

--- pages/admin/AdminLayout.tsx
+++ pages/admin/AdminLayout.tsx
@@ -1,46 +1,221 @@
-/**
- */
+import { AccountCircle } from '@mui/icons-material'
+import ApprovalIcon from '@mui/icons-material/Approval'
+import DvrIcon from '@mui/icons-material/Dvr'
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents'
+import HomeIcon from '@mui/icons-material/Home'
+import MenuIcon from '@mui/icons-material/Menu'
+import { Menu, MenuItem } from '@mui/material'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider'
+import Drawer from '@mui/material/Drawer'
+import IconButton from '@mui/material/IconButton'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import { signOut, useSession } from 'next-auth/react'
+import * as React from 'react'
 
-import { FC } from 'react'
-import { AdminSidebar } from './AdminSidebar'
-import { AdminTopBar } from './AdminTopBar'
-
+const drawerWidth = 240
 export interface AdminLayoutProps {
+  window?: () => Window
   children?: JSX.Element | JSX.Element[]
 }
 
-export const AdminLayout: FC<AdminLayoutProps> = ({ children }) => {
+export const AdminLayout: React.FC<AdminLayoutProps> = (props) => {
+  const [mobileOpen, setMobileOpen] = React.useState(false)
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const { data: session } = useSession()
+
+  const user = session?.user
+  const { window } = props
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Home'} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <EmojiEventsIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Awards'} />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <ApprovalIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Certificates'} />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <DvrIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Skills'} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </div>
+  )
+
+  const container = window !== undefined ? () => window().document.body : undefined
+
   return (
-    <>
-      <AdminTopBar />
-      <div className="md:flex flex-col md:flex-row md:min-h-screen w-full">
-        <div className="flex flex-col w-full md:w-64 text-gray-700 bg-gray-400 dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0">
-          <div className="flex-shrink-0 px-8 py-4 flex flex-row items-center justify-between">
-            <button className="rounded-lg md:hidden focus:outline-none focus:shadow-outline">
-              <svg fill="currentColor" viewBox="0 0 20 20" className="w-6 h-6">
-                <path
-                  x-show="!open"
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                />
-                <path
-                  x-show="open"
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
-          <AdminSidebar />
-        </div>
-        <main className="flex flex-col w-full bg-white dark:bg-gray-600 ">
-          <div className="flex w-full mx-auto md:p-8  ">
-            <div className="flex flex-col w-full h-full">{children}</div>
-          </div>
-        </main>
-      </div>
-    </>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      {/* APP TOP BAR */}
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` }
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            My Career
+          </Typography>
+          {user && (
+            <>
+              <div className="flex">
+                <Typography component="div" sx={{ flexGrow: 1 }}>
+                  {`Welcome, `}
+                  <span className="italic">{`${user?.firstname} ${user?.lastname}`}</span>
+                </Typography>
+              </div>
+              <div className="flex">
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right'
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right'
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem
+                    onClick={() =>
+                      signOut({
+                        callbackUrl: `/`
+                      })
+                    }
+                  >
+                    Logout
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>My account</MenuItem>
+                </Menu>
+              </div>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+      {/* DRAWERS */}
+      <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth
+            }
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth
+            }
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      {/* MAIN */}
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` }
+        }}
+      >
+        <Toolbar />
+        {props.children}
+      </Box>
+    </Box>
   )
 }
